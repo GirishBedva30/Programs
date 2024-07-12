4592fcba6441cd412f0478e96bd2402545857b95
GirishBedva30 <bca2022girish1657@tnraocollege.org>
2024-07-12 22:36:27 +0530

    Add files via upload

---
+++ program4.py
@@ -0,0 +1,7 @@
+a=["Girish","Bedva"]
+a.insert(0,"GB")
+print(a)
+a.remove("Girish")
+print(a)
+a.append("bg")
+print(a)
---
+++ program5.py
@@ -0,0 +1,6 @@
+#for 1 to 10 print
+for i in range(0,11):
+    print(i)
+#for 10 to 1 print
+for j in range(10,0,-1):
+    print(j)
